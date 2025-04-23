2df13cabd73313dfb0026df240da875d3f363081
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2025-04-23 05:49:09 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 // Check if Slug of Post or Page already exists
 function the_slug_exists( string $slug ){
   global $wpdb;
-  if( $wpdb->get_row("SELECT `post_name` FROM `wp_posts` WHERE `post_name` = '{$slug}'", 'ARRAY_A') ){
+  if( $wpdb->get_row("SELECT `post_name` FROM `{$wpdb->prefix}posts` WHERE `post_name` = '{$slug}'", 'ARRAY_A') ){
     return true;
   } else {
     return false;
